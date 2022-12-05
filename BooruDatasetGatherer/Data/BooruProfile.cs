5d56c3070a378bbd98bbd6b078fbf795d3e138e8
Velorexe <code@velorexe.com>
2022-12-05 23:05:28 +0100

    refactor(booru-profile): added whitespaces to create overview

--- BooruDatasetGatherer/Data/BooruProfile.cs
+++ BooruDatasetGatherer/Data/BooruProfile.cs
@@ -11,14 +11,17 @@ namespace BooruDatasetGatherer.Data
         [JsonPropertyName("source")]
         public string Source { get; set; } = string.Empty;
 
+
         [JsonPropertyName("filter")]
         public string[] Filter { get; set; } = Array.Empty<string>();
 
         [JsonPropertyName("files")]
         public string[] FileFilters { get; set; } = { ".png", ".jpg", ".jpeg", ".gif", ".webp" };
 
+
         [JsonPropertyName("location")]
         public string SaveLocation { get; set; } = Path.Join(AppDomain.CurrentDomain.BaseDirectory, "Images");
+
 
         [JsonPropertyName("batch")]
         public int BatchSize { get; set; } = 20;
@@ -26,8 +29,10 @@ namespace BooruDatasetGatherer.Data
         [JsonPropertyName("size")]
         public int TotalSize { get; set; } = 1000;
 
+
         [JsonPropertyName("threads")]
         public byte Threads { get; set; } = 4;
+
 
         [JsonPropertyName("nsfw")]
         public bool MatureContentAllowed { get; set; } = false;
